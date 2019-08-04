25e5ef41054751a7886ae67c934306824b226c84
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 23:35:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,11 +25,11 @@ class Application
 
     elsif req.path.match(/add/)
       searh_tern = req.params["item"]
-      if @@items.include?(search_term)
+      if !@@items.include?(search_term)
+        resp.write "We don't have that item"
+      else
         @@cart << search_term
         resp.write "added #{search_term}"
-      else
-        resp.write "We don't have that item"
       end
 
     else
