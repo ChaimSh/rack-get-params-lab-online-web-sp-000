f442a3f828bdad190f81b08c8bf3f2b4fd507665
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 23:27:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,16 @@ class Application
           resp.write "#{c}\n"
         end
       end
+
+    elsif req.path.match(/add/)
+      searh_tern = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
+      
     else
       resp.write "Path Not Found"
     end
